f976084bc8191a87e8971b69b07140a82cdd5598
Alonso <joaoalonso42@gmail.com>
2022-04-21 22:15:20 -0300

    add templates

--- flask_show/app.py
+++ flask_show/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_show.ext import site
+from flask_show.ext import toolbar
+from flask_show.ext import config
 
 def create_app():  # Factory principal
 
     app = Flask(__name__)
+    config.init_app(app)
+    toolbar.init_app(app)
     site.init_app(app)
-
     return app
